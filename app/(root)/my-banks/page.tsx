8edc0fc56bca64effe576754469615f99e22dab0
Jvsilva0800 <jvsilva13.jv@gmail.com>
2024-07-10 15:45:58 -0300

    Implementa e completa o layout

---
+++ app/(root)/my-banks/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MyBanks() {
+  return (
+    <div>banks</div>
+  )
+}
+
+export default MyBanks
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
+
 
 const Home = () => {
+    const loggedIn = {firstName: 'João', lastName: 'Victor', email: 'joaov@gamil.com'};
+
     return (
-        <div>Home</div>
+        <section className="home">
+            <div className='home-content'>
+                <header className='home-header'>
+                   <HeaderBox 
+                    type='greeting'
+                    title='Bem-vindo'
+                    user={loggedIn?.firstName || 'Guest'} 
+                    subtext='Acesse e controle sua conta e transações de maneira eficiente.'
+
+                   />
+
+                   <TotalBalanceBox 
+                    accounts={[]}
+                    totalBanks = {1}
+                    totalCurrentBalance={1500.65}
+                   />
+                </header>
+
+                Transações Recentes
+            </div>
+                <RightSidebar
+                    user={loggedIn}
+                    transactions={[]}
+                    banks={[{currentBalance: 150.4}, {currentBalance: 200.1}]}
+                />
+        </section>
     )
 }
 
